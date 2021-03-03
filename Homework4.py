e844bdab3cdaf2d6e80b5281f6b8dbd1f5088496
Susanna501 <79713391+Susanna501@users.noreply.github.com>
2021-03-03 21:35:32 +0400

    add homework4

---
+++ Homework4.py
@@ -0,0 +1,21 @@
+''' 10.You should create two good new exercises using your ideas.'''
+
+'''Task 1- The company planning to build 105 floors building, 
+every floor height will be 4.2 meter. The result convert in kilometers'''
+
+h = 4.2 
+floor = 105
+res1 = h * floor
+kilometers = 1000 	#1 Kilometer = 1000 Meters, 1m =0.001km
+result = res1 / kilometers
+print(result)
+
+'''Task 2- Gas utility cost is 55 000,calculate how cub meter gas cost is?(convert to AMD), 
+where 1 cub meter gas is 1.5$'''
+
+dollar_1 = 522.5     		# 1usd = 522.5 amd  
+cub_meter = 1.5 			# 1cb m = 1.5 usd
+res = cub_meter * dollar_1
+utility = 55000 / res
+print(utility)
+
