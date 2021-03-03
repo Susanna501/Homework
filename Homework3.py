6c1a2e2c46076c816b2f5225ece724a080f2eb8f
Susanna501 <79713391+Susanna501@users.noreply.github.com>
2021-03-03 21:36:11 +0400

    add homework3

---
+++ Homework3.py
@@ -0,0 +1,83 @@
+
+'''Task1- Write code to print pypypypython'''
+name1 = 'thon'
+print('py' * 4 +name1)
+
+
+'''Task2- Write code to print 7p7p7p79'''
+name2 = '79'
+print('7p' * 3 + name2)
+
+
+'''Task3- Write code to print 9999999'''
+name3 = '9'
+print(name3 * 7)
+
+
+'''Task4- Write a Python program that checks how much money you have on your card.
+At first, your balance is 0, and you have 3 inputs and add each of them to the card.'''
+money1 = int(input('money1: '))
+money2 = int(input('money2: '))
+money3 = int(input('money3: '))
+balance = 0
+card = balance + money1 + money2 +money3
+print(card)
+
+'''Task5- Create 7 legal variables and check type of them.'''
+firstname = 'Susanna'
+print(type(firstname))
+age = 22
+print(type(age))
+weight = 20.4
+print(type(weight))
+country = 'Armenia'
+print(type(country))
+year_20 = 2013
+print(type(year_20))
+language_arm = True
+print(type(language_arm))
+music_band_2 = 'SOAD'
+print(type(music_band_2))
+
+
+'''Task6-Create 10 variables integer and float and compare this variables.'''
+age_1 = 20
+print(age_1)
+year_1 = 2021
+print(year_1)
+zipcode = 19203
+print(zipcode)
+city_code = 1503
+print(city_code)
+bank_account = 22700
+print(bank_account)
+weight_1 = 15.42
+print(weight_1)
+celsius_1 = 24.5
+print(celsius_1)
+celsius_2 = 31.25
+print(celsius_2)
+kg = 10.32
+print(kg)
+gram = 1056.34
+print(gram)
+
+
+'''Task7-Create a python program which convert the pound in kilograms.'''
+pound1= 0.45359237 #(1 pound =0.45359237kg)
+kg = float(input('kg: '))
+res = kg * pound1
+print(res)
+
+
+'''Task8- Create a python program which convert the miles in kilometers. '''
+miles = 1.609344 #(1 miles = 1.609344 km)
+km = float(input('km: '))
+result = km * miles
+print(result)
+
+
+
+
+
+
