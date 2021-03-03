555d08f4a11dbc0afbaa0a838fc53dc5f1e891f0
Susanna501 <79713391+Susanna501@users.noreply.github.com>
2021-03-03 21:37:02 +0400

    add homework2

---
+++ Homework2.py
@@ -0,0 +1,50 @@
+
+# Task1- Write a python program which have two input and compute the area of a rectangle.
+l = float(input('length '))
+w = float(input('width '))
+a = l * w
+print('Area is',a)
+
+
+# Task2- Write a python program which have one input and compute the area of a square. 
+a = float(input('area '))
+A = a ** 2
+print('Area of a square is', A)
+
+
+# Task3- Create 20 legal variables. for example: country = “Armenia”
+name = 'Susik'
+age = 15
+country = 'Armenia'
+region = 'Caucusus'
+loc_cnt = 1001
+loc_reg = 152
+flag = 'Red'
+square = 29750.3
+friend1 = 'Ani'
+friend2 = 'mama'
+my_car = 'Jac'
+course_1 = 'sql'
+course_2 = 'power BI'
+course_3 = 'python'
+age_mycar = 2024
+age_mymom = 50
+new_color = 'Black'
+model = 'bronze'
+model_year = 1994
+my_city = 'Yerevan'
+
+
+# Task4- Write a python program which will check the type of your 
+# variables.(int, str , float , bool)
+l = 4.53
+print(type(l))
+w = 20
+print(type(20))
+first_name = 'Susanna'
+print(type(first_name))
+check_bool = True
+print(type(check_bool))
+  
+ 
+ 
