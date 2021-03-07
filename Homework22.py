645a9215d5054c2b4d6288b6f7bad4781f109313
Susanna501 <79713391+Susanna501@users.noreply.github.com>
2021-03-08 00:07:04 +0400

    add homework22

---
+++ Homework22.py
@@ -0,0 +1,74 @@
+'''1. km to mile Write a python function which conversion kilometer to miles.'''
+# def km_to_miles(km):
+# 	return km / 1.6     # or   km * 0.621371
+
+# print(km_to_miles(8))
+
+
+
+
+'''2. Time Write a python function which conversion days to seconds.'''
+# def days_to_second(seconds):
+# 	return seconds * 24 * 60 * 60    
+
+# print(days_to_second(3))
+
+
+
+
+'''3. Password Write a python function which generate a valid password.'''
+# def pas(password):
+# 	count = 0
+# 	for i in password:
+# 		if i.isdigit():
+# 			count += 1
+# 	if password[0].isupper() and len(password) >= 8 and count > 1:
+# 		return 'Valid password'
+# 	else:
+# 		return 'Not Valid password'
+
+# my_password = input('enter your pass: ')
+
+# print(pas(my_password))
+
+
+
+
+'''4. Factorial Factorial'''
+
+# def my_factorial(factorial):
+
+# 	count = 1
+# 	end = num  + 1
+# 	for i in range(1,end):
+# 		count *= i
+# 	return count
+
+# num = int(input("My number is: " ))
+
+# print(my_factorial(num))
+
+
+
+
+'''5. Max Given an list of numbers. Find the maximum element in list. Without use max function.'''
+
+# def my_list_(num):
+	
+# 	maxim = num[0]
+
+# 	for i in num:
+# 		if i > maxim:
+# 			maxim = i
+# 	return maxim
+
+# num = [50, 16, -105, 69, -2, 149]
+# print(my_list_(num))
+
+
+
+
+
+
+
+
